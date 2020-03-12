b831860fd81d0d97b2bf3e0a6be6514ef61301f8
Mika Caldera <mika.caldera@gmail.com>
2020-03-12 16:56:26 -0400

    Create plugin.php
    Adds code.

---
+++ plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name: Rebel Category for Blocks
+ * Plugin URI: 
+ * Description: Plugin to add a custom block type for Gutenburg Blocks Category
+ * Version: 1.0
+ * Author: Mika Caldera
+ * Author URI: https://www.mikacaldera.com/
+ */
+
+ // Adds functionality of a block category - 
+ 
+ /**
+ * Custom Rebel Block Category
+ */
+add_filter('block_categories', function ($categories, $post) {
+  return array_merge(
+      $categories,
+      array(
+          array(
+              'slug' => 'rebel',
+              'title' => __('Custom Rebel', 'rebel'),
+          ),
+      )
+  );
+}, 1, 2);
