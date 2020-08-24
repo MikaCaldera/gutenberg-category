7703cb945e134c9d20cf9e37b4c238e4cdea61eb
Mika Caldera <mika.caldera@gmail.com>
2020-08-24 09:37:59 -0400

    :wrench: Clean up Clean up

--- README.md
+++ README.md
@@ -3,18 +3,18 @@
 Designed for custom WordPress Gutenburg blocks using sage and roots.io
 
 ### :electric_plug: Install
-Add following snippet to your site: 
+Add following snippet to your site:
 
     `{
 	"type": "vcs",
-	"url": "git@github.com:MikaCaldera/rebel-category.git"
+	"url": "git@github.com:MikaCaldera/gutenberg-category.git"
 	}`
 
 
 In your site folder Run the following command.
 
-	`composer require "mikacaldera/rebel-category"`
+	`composer require "mikacaldera/gutenberg-category"`
 
 ## :triangular_ruler: Use
 
--   When you create a block use the category  `rebel`  to have it listed under custom Rebel bocks
+-   When you create a block use the category  `custom`  to have it listed under custom Rebel bocks
--- plugin.php
+++ plugin.php
@@ -1,25 +1,25 @@
 <?php
 /**
- * Plugin Name: Rebel Category for Blocks
- * Plugin URI: 
+ * Plugin Name: Gutenburg Category for Blocks
+ * Plugin URI:
  * Description: Plugin to add a custom block type for Gutenburg Blocks Category
  * Version: 1.0
  * Author: Mika Caldera
  * Author URI: https://www.mikacaldera.com/
  */
 
- // Adds functionality of a block category - 
- 
+ // Adds functionality of a block category -
+
  /**
- * Custom Rebel Block Category
+ * Custom Gutenburg Block Category
  */
 add_filter('block_categories', function ($categories, $post) {
   return array_merge(
       $categories,
       array(
           array(
-              'slug' => 'rebel',
-              'title' => __('Custom Rebel', 'rebel'),
+              'slug' => 'custom',
+              'title' => __('Custom', 'custom'),
           ),
       )
   );
