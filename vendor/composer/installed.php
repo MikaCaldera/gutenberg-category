f0e394187766ec51b8b573833a242fb6e07ed6d3
Mika Caldera <mika.caldera@gmail.com>
2021-04-02 13:20:17 -0400

    :alien: update code

---
+++ vendor/composer/installed.php
@@ -0,0 +1,47 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6df1f9bcba7b4161c204cfb189b98ec2ade037b7',
+    'name' => 'mikacaldera/custom-category',
+  ),
+  'versions' => 
+  array (
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.10.0',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1a0357fccad9d1cc1ea0c9a05b8847fbccccb78d',
+    ),
+    'mikacaldera/custom-category' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6df1f9bcba7b4161c204cfb189b98ec2ade037b7',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+  ),
+);
